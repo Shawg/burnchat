1cc8ca51e48be52d43d68a03972079394d97ad85
geoffshaw <geoffshawbc@gmail.com>
2016-11-20 17:45:51 -0800

    Open invite needed to join room

--- public/client.js
+++ public/client.js
@@ -6,6 +6,11 @@ $('form').submit(function(){
   return false;
 });
 
+$('#invite').click(function(){
+  alert('send friend this url: localhost:8000/chat/123');
+  socket.emit('invite sent');
+});
+
 socket.on('chat message', function(msg){
   msg = sjcl.decrypt("password", msg);
   $('#messages').append($('<li>').text(msg));
--- server.js
+++ server.js
@@ -4,24 +4,53 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-numChatters = 0;
+//the number of participants in the room
+curr = 0;
+//the total number of participants allowed
+limit = 1
 
 app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res,next) {
-  res.sendFile(__dirname + '/index.html');
+  if(curr < limit){
+    res.sendFile(__dirname + '/index.html');
+  } else {
+    res.redirect('/full');
+  }
 });
 
+app.get('/chat/:id', function(req, res,next) {
+  if(curr < limit){
+    res.sendFile(__dirname + '/index.html');
+  } else {
+    res.redirect('/full');
+  }
+});
+
+app.get('/full', function(req, res, next) {
+  res.sendFile(__dirname + '/full.html');
+})
+
 io.on('connection', function(socket){
-  numChatters = numChatters + 1;
-  console.log('a user connected, '+numChatters+' connected');
+  curr = curr + 1;
+  console.log(curr+' connected');
+
   socket.on('disconnect', function(){
-    numChatters = numChatters - 1;
+    curr = curr - 1;
+    limit = limit - 1;
+    if (limit == 0) {
+      limit = 1;
+    }
     console.log('user disconnected');
   });
 
   socket.on('chat message', function(msg){
     console.log(msg);
     io.emit('chat message', msg);
+  });
+
+  socket.on('invite', function(){
+    limit = limit+1;
   });
 });
 
